acec31501b41195c7703a60de129c76a34a5824e
troy.wagonera <troy.wagonera@gmail.com>
2021-11-03 14:46:11 -0400

    added CRUD and switchcase

--- Hero.php
+++ Hero.php
@@ -1,3 +1,4 @@
+<?php
 // connect to database
 // switch for a route
 // run a CRUD functino
@@ -6,3 +7,57 @@
 //      inside function, return result of query
 // echo data to api on postman
 // close connection 
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "SQL_Hero";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+
+
+// define functions
+function create() {
+    print_r ($_GET);
+    $name = 'Ian';
+    $about_me = 'coder';
+    $biography = 'ahdjkal';
+    $SQL = "INSERT INTO heroes (name, about_me, biography) 
+            VALUES ('$name', '$about_me', '$biography')";
+    echo $SQL;
+}
+
+
+
+
+
+// codeanywhere.bhalbkah.com/Hero_API/Hero.php?action=read
+if (isset($_GET['action'])) {
+    $action = $_GET['action'];
+    switch ($action) {
+        case "create":
+            create();
+            break;
+        case "read":
+            echo "read thing";
+            break;
+        case "update":
+            echo "updated thing";
+            break;
+        case "delete":
+            echo "deleted thing";
+            break;    
+        default:
+            echo "init thing";
+    }
+}
+else{
+    // action not set, do something
+}
