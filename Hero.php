0effa7bf49d854ab1f0b110af948d5cbdb27117a
troy.wagonera <troy.wagonera@gmail.com>
2021-11-03 11:31:51 -0400

    added php file and some psuedocode

---
+++ Hero.php
@@ -0,0 +1,8 @@
+// connect to database
+// switch for a route
+// run a CRUD functino
+//      inside function, check for errors
+//      inside function, access database for the CRUD function - run a mySQL query
+//      inside function, return result of query
+// echo data to api on postman
+// close connection
