5980ac9011432c526813f68f2b36436192603183
troy.wagonera <troy.wagonera@gmail.com>
2021-11-04 17:01:10 -0400

    added crud ops and the work.

--- Hero.php
+++ Hero.php
@@ -24,26 +24,71 @@ echo "Connected successfully";
 
 
 // define functions
-function create($name, $about_me, $biography) {
-    print_r ($_GET);
-    
+function create(){
+
+    $name = $_POST['name'];
+    $about_me = $_POST['about_me'];
+    $biography = $_POST['biography'];
     $SQL = "INSERT INTO heroes (name, about_me, biography) 
             VALUES ('$name', '$about_me', '$biography')";
-    echo $SQL;
+
+    global $conn;
+
+    if ($conn->query($SQL) === TRUE) {
+        echo "New record created successfully";
+    } else {
+        echo "Error: " . $SQL . "<br>" . $conn->error;
+    }
 }
-function read() {
+function read()
+{
 
-    $SQL = "SELECT name, about_me, biography FROM heroes WHERE 1";
+    $SQL = "SELECT name, about_me, biography FROM heroes";
 
-    echo $SQL;
+    global $conn;
+
+    $result = $conn->query($SQL);
+
+    if ($result->num_rows > 0) {
+        // output data of each row
+        while ($row = $result->fetch_assoc()) {
+            echo " - Name: " . $row["name"] . " " . $row["about_me"] . $row["biography"] . " " . "<br>";
+        }
+    } else {
+        echo "0 results";
+    }
 }
 
-function update($name, $about_me, $biography) {
-    $SQL = "UPDATE heroes SET name=[$name],about_me=[$about_me],biography=[$biography] WHERE 1";
+function update()
+{
+    $id = $_POST['id'];
+    $name = $_POST['name'];
+    $about_me = $_POST['about_me'];
+    $biography = $_POST['biography'];
+    $SQL = "UPDATE heroes SET name='$name', about_me='$about_me', biography='$biography' WHERE heroes.id = $id";
 
-    echo $SQL;
+    global $conn;
+
+    if ($conn->query($SQL) === TRUE) {
+        echo "record updated successfully";
+    } else {
+        echo "Error: " . $SQL . "<br>" . $conn->error;
+    }
 }
 
+function delete()
+{
+    $id = $_GET['id'];
+    $SQL = "DELETE FROM heroes WHERE heroes.id = $id";
+
+    global $conn;
+
+    if ($conn->query($SQL) === TRUE) {
+        echo "record deleted successfully";
+    } else {
+        echo "Error: " . $SQL . "<br>" . $conn->error;
+    }
+}
 
 // codeanywhere.bhalbkah.com/Hero_API/Hero.php?action=read
 if (isset($_GET['action'])) {
@@ -56,15 +101,70 @@ if (isset($_GET['action'])) {
             read();
             break;
         case "update":
-            update($name, $about_me, $biography);
+            update($name, $about_me, $biography, $id);
             break;
         case "delete":
-            echo "deleted thing";
-            break;    
+            delete();
+            break;
         default:
             echo "init thing";
     }
-}
-else{
+} else {
     // action not set, do something
 }
+
+
+
+
+
+
+
+
+// class DatabaseConnector {
+//    private $servername = "localhost";
+//    private $username = "root";
+//    private $password = "";
+//    private $dbname = "SQL_Hero";
+//    private $conn;
+//    private $heroes;
+   
+//     function connect() {
+//         $this->conn = new mysqli ($this->servername, $this->username, $this->password, $this->dbname);
+//     }
+
+//     function disconnect() {
+//        $this->conn->close();
+//     }
+
+//     function getAllHeroes() {
+    
+//         $this->connect();
+//         $SQL = "SELECT * FROM heroes";
+//         $result = $this->conn ->query($SQL);
+
+//         if($result->num_rows > 0) {
+//             while($row = $result->fetch_assoc()) {
+//                 $heroObj = [];
+//                 array_push($this->heroes, $heroObj);
+//             }
+//         } else {
+//             $this ->heroes = [];
+//         }
+//         $this->disconnect();
+//         return $this->heros;
+//     }
+    
+
+// }
+
+
+//  function runSQL($sql){
+//         // run the sql command
+//     }
+//     function getAllHeroes($sql){
+//         $results = null;
+
+//         //
+
+
+//         return $results;
